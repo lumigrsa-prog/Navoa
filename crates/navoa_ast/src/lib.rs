051f352de1354f3519e701e71d117a05fbc26342
O teu nome <teu_email@exemplo.com>
2026-09-22 01:09:20 +0100

    Definir Statement e Expr no crate navoa_ast

--- crates/navoa_ast/src/lib.rs
+++ crates/navoa_ast/src/lib.rs
@@ -1,22 +1,12 @@
-use navoa_lexer::Token;
-
 #[derive(Debug, Clone, PartialEq)]
 pub enum Expr {
-    Number(f64),
-    String(String),
-    Variable(String),
-    Binary {
-        left: Box<Expr>,
-        operator: Token,
-        right: Box<Expr>,
-    },
+    Numero(f64),
+    Texto(String),
+    Identificador(String),
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum AstStmt {
-    VarDecl { name: String, value: Expr },
-    Print(Expr),
-    Assign { name: String, value: Expr },
-    Block(Vec<AstStmt>),
-    While { condition: Expr, body: Box<AstStmt> },
+pub enum Statement {
+    Imprimir(Expr),
+    Atribuir(String, Expr),
 }
